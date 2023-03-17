8dddff9e2dc573adbeb22d1b8801f10b9a14c2c4
GageVanK <87737875+GageVanK@users.noreply.github.com>
2023-03-16 23:37:04 -0400

    Clean up comments

--- src/main/java/javaClasses/ToDoMethods.java
+++ src/main/java/javaClasses/ToDoMethods.java
@@ -3,18 +3,23 @@ package javaClasses;
 import java.util.ArrayList;
 
 public class ToDoMethods {
-	static ArrayList<String> todo = new ArrayList<String>(); //ArrayList of Strings for stroing todo Item
+	//ArrayList of Strings for storing todo items
+	static ArrayList<String> todo = new ArrayList<String>(); 
 
-	public static String addItem(String item){ //function for adding item
+	//function for adding item
+	public static String addItem(String item){ 
 			todo.add(item);
 	        return item;
 	}
-
-	public void deleteItem(int num){ //function for deleting item
+	
+	
+	//function for deleting item
+	public void deleteItem(int num){ 
 	        todo.remove(num);
 	}
-	        
-	public void listAll(){ //function for listing all itemsToDoList todo=new ToDoList();ToDoList todo=new ToDoList();
+	
+	//function for listing all itemsToDoList todo=new ToDoList();ToDoList todo=new ToDoList();
+	public void listAll(){ 
 	        for (int i = 0; i < todo.size(); i++) {
 	      System.out.println((i+1)+"."+todo.get(i));
 	    }
